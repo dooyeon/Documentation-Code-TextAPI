54501b548a1e0adcc379d15406991622d69ca0f5
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2017-10-30 20:37:32 -0700

    Create GetLanguagesForTranslate.js

---
+++ NodeJS/GetLanguagesForTranslate.js
@@ -0,0 +1,44 @@
+'use strict';
+
+let https = require ('https');
+
+// **********************************************
+// *** Update or verify the following values. ***
+// **********************************************
+
+// Replace the subscriptionKey string value with your valid subscription key.
+let subscriptionKey = 'ENTER KEY HERE';
+
+let host = 'api.microsofttranslator.com';
+let path = '/V2/Http.svc/GetLanguagesForTranslate';
+
+let params = '';
+
+let response_handler = function (response) {
+    let body = '';
+    response.on ('data', function (d) {
+        body += d;
+    });
+    response.on ('end', function () {
+		console.log (body);
+    });
+    response.on ('error', function (e) {
+        console.log ('Error: ' + e.message);
+    });
+};
+
+let GetLanguagesForTranslate = function () {
+	let request_params = {
+		method : 'GET',
+		hostname : host,
+		path : path + params,
+		headers : {
+			'Ocp-Apim-Subscription-Key' : subscriptionKey,
+		}
+	};
+
+	let req = https.request (request_params, response_handler);
+	req.end ();
+}
+
+GetLanguagesForTranslate ();
