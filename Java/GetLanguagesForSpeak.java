af39c29404ec11c2a2e0417833cd3086f5330de7
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2017-10-30 20:32:14 -0700

    Create GetLanguagesForSpeak.java

---
+++ Java/GetLanguagesForSpeak.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javax.net.ssl.HttpsURLConnection;
+
+public class GetLanguagesForSpeak {
+
+// **********************************************
+// *** Update or verify the following values. ***
+// **********************************************
+
+// Replace the key string value with your valid subscription key.
+	static String key = "ENTER KEY HERE";
+
+	static String host = "https://api.microsofttranslator.com";
+	static String path = "/V2/Http.svc/GetLanguagesForSpeak";
+
+	public static String get (URL url) throws Exception {
+		HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+		connection.setRequestProperty("Ocp-Apim-Subscription-Key", key);
+		connection.setDoOutput(true);
+
+		StringBuilder response = new StringBuilder ();
+		BufferedReader in = new BufferedReader(
+		new InputStreamReader(connection.getInputStream()));
+		String line;
+		while ((line = in.readLine()) != null) {
+			response.append(line);
+		}
+		in.close();
+
+		return response.toString();
+	}
+
+	public static String GetLanguagesForSpeak () throws Exception {
+		String url = host + path;
+		URL url2 = new URL (url);
+
+		return get (url2);
+    }
+
+	public static void main(String[] args) {
+		try {
+			String response = GetLanguagesForSpeak ();
+			System.out.println (response);
+		}
+		catch (Exception e) {
+			System.out.println (e);
+		}
+	}
+}
