0c6e69540df6546d1eb5f5bded822a90e8ecf34c
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2017-10-30 21:22:58 -0700

    Create BreakSentences.php

---
+++ PHP/BreakSentences.php
@@ -0,0 +1,45 @@
+<?php
+
+// NOTE: Be sure to uncomment the following line in your php.ini file.
+// ;extension=php_openssl.dll
+
+// **********************************************
+// *** Update or verify the following values. ***
+// **********************************************
+
+// Replace the subscriptionKey string value with your valid subscription key.
+$key = 'ENTER KEY HERE';
+
+$host = "https://api.microsofttranslator.com";
+$path = "/V2/Http.svc/BreakSentences";
+
+$text = "Here is a sentence. Here is another sentence. Here is a third sentence.";
+$language = "en-US";
+
+$params =
+	"?text=" . urlencode ($text) .
+	"&language=" . $language;
+
+function BreakSentences ($host, $path, $key, $params) {
+
+	$headers =
+		"Content-type: text/xml\r\n" .
+		"Ocp-Apim-Subscription-Key: $key\r\n";
+
+	// NOTE: Use the key 'http' even if you are making an HTTPS request. See:
+	// http://php.net/manual/en/function.stream-context-create.php
+	$options = array (
+		'http' => array (
+			'header' => $headers,
+			'method' => 'GET'
+		)
+	);
+	$context  = stream_context_create ($options);
+	$result = file_get_contents ($host . $path . $params, false, $context);
+	return $result;
+
+}
+
+$result = BreakSentences ($host, $path, $key, $params);
+echo $result;
+?>
