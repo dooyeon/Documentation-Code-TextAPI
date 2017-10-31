95aa6432ba891c1ae6fc7bd278d309ee167e53dd
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2017-10-30 20:24:45 -0700

    Create GetLanguagesForSpeak.cs

---
+++ CSharp/GetLanguagesForSpeak.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace TranslateTextQuickStart
+{
+    class Program
+    {
+        static string host = "https://api.microsofttranslator.com";
+        static string path = "/V2/Http.svc/GetLanguagesForSpeak";
+
+        // NOTE: Replace this example key with a valid subscription key.
+        static string key = "ENTER KEY HERE";
+
+        async static void GetLanguagesForSpeak()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", key);
+
+            string uri = host + path;
+
+            HttpResponseMessage response = await client.GetAsync(uri);
+
+            string result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result);
+
+            // NOTE: Use the following code to deserialize the stream contents.
+            /*
+            DataContractSerializer dcs = new DataContractSerializer(Type.GetType("System.String[]"));
+            MemoryStream memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            string[] languageNames = (string[])dcs.ReadObject(memoryStream);
+            foreach (var i in languageNames)
+            {
+                Console.WriteLine(i);
+            }
+            */
+        }
+
+        static void Main(string[] args)
+        {
+            GetLanguagesForSpeak();
+            Console.ReadLine();
+        }
+    }
+}
