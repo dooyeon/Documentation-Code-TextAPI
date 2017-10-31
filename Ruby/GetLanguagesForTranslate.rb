d037309a9b875beefa4f07de9465e1100602b388
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2017-10-30 21:25:04 -0700

    Create GetLanguagesForTranslate.rb

---
+++ Ruby/GetLanguagesForTranslate.rb
@@ -0,0 +1,26 @@
+require 'net/https'
+require 'uri'
+require 'cgi'
+
+# **********************************************
+# *** Update or verify the following values. ***
+# **********************************************
+
+# Replace the key string value with your valid subscription key.
+key = 'ENTER KEY HERE'
+
+host = 'https://api.microsofttranslator.com'
+path = '/V2/Http.svc/GetLanguagesForTranslate'
+
+params = ''
+
+uri = URI (host + path + params)
+
+request = Net::HTTP::Get.new(uri)
+request['Ocp-Apim-Subscription-Key'] = key
+
+response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+    http.request (request)
+end
+
+puts response.body
