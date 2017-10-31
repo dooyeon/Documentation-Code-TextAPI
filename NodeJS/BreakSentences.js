011c6d20db03ee985683abccff9b0b24c02f8549
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2017-10-30 20:39:53 -0700

    Create BreakSentences.js

---
+++ NodeJS/BreakSentences.js
@@ -0,0 +1,49 @@
+'use strict';
+
+let https = require ('https');
+
+// **********************************************
+// *** Update or verify the following values. ***
+// **********************************************
+
+// Replace the subscriptionKey string value with your valid subscription key.
+let subscriptionKey = 'ENTER KEY HERE';
+
+let host = 'api.microsofttranslator.com';
+let path = '/V2/Http.svc/BreakSentences';
+
+let text = "Here is a sentence. Here is another sentence. Here is a third sentence.";
+let language = "en-US";
+
+let params =
+	"?text=" + encodeURI (text) +
+	"&language=" + language;
+
+let response_handler = function (response) {
+    let body = '';
+    response.on ('data', function (d) {
+        body += d;
+    });
+    response.on ('end', function () {
+		console.log (body);
+    });
+    response.on ('error', function (e) {
+        console.log ('Error: ' + e.message);
+    });
+};
+
+let BreakSentences = function () {
+	let request_params = {
+		method : 'GET',
+		hostname : host,
+		path : path + params,
+		headers : {
+			'Ocp-Apim-Subscription-Key' : subscriptionKey,
+		}
+	};
+
+	let req = https.request (request_params, response_handler);
+	req.end ();
+}
+
+BreakSentences ();
