ef842bd9e8acfd4c008f339f756eae5ceaac6188
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2017-10-30 21:18:46 -0700

    Create TranslateArray.php

---
+++ PHP/TranslateArray.php
@@ -0,0 +1,52 @@
+<?php
+
+// NOTE: Be sure to uncomment the following line in your php.ini file.
+// ;extension=php_openssl.dll
+
+// **********************************************
+// *** Update or verify the following values. ***
+// **********************************************
+
+// Replace the subscriptionKey string value with your valid subscription key.
+$key = 'ENTER KEY HERE';
+
+$host = "https://api.microsofttranslator.com";
+$path = "/V2/Http.svc/TranslateArray";
+
+$params = '';
+
+$ns = "http://schemas.microsoft.com/2003/10/Serialization/Arrays";
+$content =
+	"<TranslateArrayRequest>" .
+	// NOTE: AppId is required, but it can be empty because we are sending the Ocp-Apim-Subscription-Key header.
+	"	<AppId />" .
+	"	<Texts>" .
+	"		<string xmlns=\"$ns\">Hello</string>" .
+	"		<string xmlns=\"$ns\">Goodbye</string>" .
+	"	</Texts>" .
+	"	<To>fr-fr</To>" .
+	"</TranslateArrayRequest>";
+
+function TranslateArray ($host, $path, $key, $params, $content) {
+
+	$headers = "Content-type: text/xml\r\n" .
+		"Ocp-Apim-Subscription-Key: $key\r\n";
+
+	// NOTE: Use the key 'http' even if you are making an HTTPS request. See:
+	// http://php.net/manual/en/function.stream-context-create.php
+	$options = array (
+		'http' => array (
+			'header' => $headers,
+			'method' => 'POST',
+			'content' => $content
+		)
+	);
+	$context  = stream_context_create ($options);
+	$result = file_get_contents ($host . $path . $params, false, $context);
+	return $result;
+}
+
+$result = TranslateArray ($host, $path, $key, $params, $content);
+
+echo $result;
+?>
