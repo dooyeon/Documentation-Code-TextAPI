38ca046c757ed550b35b8ac9002cad33165926c1
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2017-10-30 21:26:20 -0700

    Create DetectArray.rb

---
+++ Ruby/DetectArray.rb
@@ -0,0 +1,35 @@
+require 'net/https'
+require 'uri'
+require 'cgi'
+
+# **********************************************
+# *** Update or verify the following values. ***
+# **********************************************
+
+# Replace the key string value with your valid subscription key.
+key = 'ENTER KEY HERE'
+
+host = 'https://api.microsofttranslator.com'
+path = '/V2/Http.svc/DetectArray'
+
+params = ''
+
+body =
+	"<ArrayOfstring xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://schemas.microsoft.com/2003/10/Serialization/Arrays\">" +
+	"    <string>Hello</string>" +
+	"    <string>Bonjour</string>" +
+	"    <string>Guten tag</string>" +
+	"</ArrayOfstring>"
+
+uri = URI (host + path + params)
+
+request = Net::HTTP::Post.new(uri)
+request['Content-type'] = 'text/xml'
+request['Ocp-Apim-Subscription-Key'] = key
+request.body = body
+
+response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+    http.request (request)
+end
+
+puts response.body
